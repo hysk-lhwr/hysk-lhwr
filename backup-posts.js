b46607031684ae2197bb2f49a729e170fc2e201c
hysk-lawr <joebeav@me.com>
2020-12-17 12:59:14 -0500

    setup action env and add posts download script

---
+++ backup-posts.js
@@ -0,0 +1,60 @@
+const process = require('process');
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+let Parser = require('rss-parser');
+const keymap = require('./keymap.json');
+const sourceLink = core.getInput('source-link');
+const postsPath = core.getInput('posts-path');
+const exec = require('./exec');
+const GITHUB_TOKEN = core.getInput('gh_token');
+const oldPosts = require(postsPath);
+
+const commitPosts = async () => {
+  // Getting config
+  const committerUsername = 'posts-backup-workflow';
+  const committerEmail = 'posts.backup.workflow@example.com';
+  const commitMessage = 'update my-posts.json';
+  await exec('git', [
+    'config',
+    '--global',
+    'user.email',
+    committerEmail,
+  ]);
+  if (GITHUB_TOKEN) {
+    // git remote set-url origin
+    await exec('git', ['remote', 'set-url', 'origin',
+      `https://${GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`
+      ]);
+  }
+  await exec('git', ['config', '--global', 'user.name', committerUsername]);
+  await exec('git', ['add', postsPath]);
+  await exec('git', ['commit', '-m', commitMessage]);
+  await exec('git', ['push']);
+};
+
+new Parser().parseURL(sourceLink).then(data => {
+    if(!data.items) {
+        return;
+    } else {
+        var posts = [];
+        for(item of data.items) {
+            posts.push({
+                title: item[keymap['douban'].title],
+                author: item[keymap['douban'].author],
+                dateCreated: item[keymap['douban'].dateCreated],
+                canonicalLink: item[keymap['douban'].canonicalLink],
+                contentFormatted: item[keymap['douban'].contentFormatted],
+                contentText: item[keymap['douban'].contentText],
+                abstract: item[keymap['douban'].abstract]
+            });
+        }
+        console.log('writing posts');
+        if (JSON.stringify(posts) !== JSON.stringify(oldPosts)) {
+            fs.writeFileSync(postsPath, JSON.stringify(posts));
+            if (false) {
+                commitPosts().then().catch();
+            }
+        }
+    }
+}).catch();
